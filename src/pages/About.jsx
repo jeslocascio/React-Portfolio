638e4570fa19bd3d5950888a3a7b4ac3e2a501e2
jeslocascio <locascioje@gmail.com>
2023-11-19 19:42:27 -0500

    Set up basis of About Me (Will be Updated)

---
+++ src/pages/About.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function About() {
+  return (
+    <>
+      <img src="https://via.placeholder.com/150" alt="placeholder" />
+      <h2>About Me</h2>
+      <p>
+        Hello! My name is Jesse LoCascio. Currently, I'm on an exciting journey
+        as a full-stack web developer. My path began with a strong foundation in
+        the arts, as I hold a BFA in Visual Arts with a concentration in Visual
+        Communications (in simpler terms, I have a Graphic Design degree). This
+        creative background fuels my passion for web development, where design
+        and code come together in a really enticing way.
+        <br></br>My journey has been a dynamic one. I've been a pioneer in the
+        pilot program for an AP Computer Science class in high school, led club
+        events and workshops as a club president in college, and eagerly
+        attended a range of technology conventions after college. Each
+        experience has been a stepping stone in my pursuit of excellence in this
+        new field. As I move forward, I'm committed to providing fresh insights
+        and innovative solutions to the ever-evolving world of web development.
+        Join me as we explore the limitless possibilities of this digital
+        landscape.
+      </p>
+    </>
+  );
+}
+
+export default About;
